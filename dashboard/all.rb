4ae2b206001c62075aa08dabef8b8ad77b6d4a21
Anthony Tesija <antesija@gmail.com>
2019-03-27 08:15:06 -0700

    Get 5 and 5R bus times

--- dashboard/all.rb
+++ dashboard/all.rb
@@ -1,12 +1,21 @@
 require 'net/http'
 require 'json'
 
-def get_predictions(url)
-    uri = URI(url)
-    JSON.parse(Net::HTTP.get(uri))#&.dig('prediction')
+def get_predictions(bus_number, stop_number)
+    uri = URI("http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=sf-muni&r=#{bus_number}&s=#{stop_number}")
+    JSON.parse(Net::HTTP.get(uri))
 end
 
-five_predictions = get_predictions('http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=sf-muni&r=5&s=4224')
-five_r_predictions = get_predictions('http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=sf-muni&r=5R&s=4224')
+five_predictions = get_predictions('5', 4224)&.dig('predictions')&.dig('direction')&.dig('prediction')
+five_r_predictions = get_predictions('5R', 4224)&.dig('predictions')&.dig('direction')&.dig('prediction')
+
+times = []
+(five_predictions + five_r_predictions).each do |prediction|
+    times.append(prediction['minutes'].to_i)
+end
+
+p times.sort
 
 #five_predictions&.dig('predictions')&.dig('direction')&.dig('prediction')[0]&.dig('minutes')
+
+#http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&r=5&s=4224
