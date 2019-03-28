4f7dbb4fa773ccaced2d0b3bd0995620a1a349de
Anthony Tesija <antesija@gmail.com>
2019-03-27 21:18:51 -0700

    Dig into prediction times

--- dashboard/all.rb
+++ dashboard/all.rb
@@ -1,13 +1,27 @@
 require 'net/http'
 require 'json'
 
-def get_predictions(bus_number, stop_number)
-    uri = URI("http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=sf-muni&r=#{bus_number}&s=#{stop_number}")
-    JSON.parse(Net::HTTP.get(uri))
+class Hash
+    def deep_find(key, object=self, found=[])
+        if object.respond_to?(:key?) && object.key?(key)
+            found << object[key]
+            return found.flatten
+        elsif object.is_a? Enumerable
+            object.each { |*a| deep_find(key, a.last, found) }
+            return found.flatten
+        end
+    end
 end
 
-five_predictions = get_predictions('5', 4224)&.dig('predictions')&.dig('direction')&.dig('prediction')
-five_r_predictions = get_predictions('5R', 4224)&.dig('predictions')&.dig('direction')&.dig('prediction')
+def get_predictions(bus_number, stop_number)
+    uri = URI("http://webservices.nextbus.com/service/publicJSONFeed?command=predictions&a=sf-muni&r=#{bus_number}&s=#{stop_number}")
+    predictions = JSON.parse(Net::HTTP.get(uri)).deep_find("prediction")
+    z = predictions.map { |p| {bus_number => p&.dig('minutes')&.to_i } }
+    z.sort_by { |hsh| hsh[bus_number] }
+end
+
+five_predictions = get_predictions('5', 4224)
+five_r_predictions = get_predictions('5R', 4224)
 
 times = []
 (five_predictions + five_r_predictions).each do |prediction|
