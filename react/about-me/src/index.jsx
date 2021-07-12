b5caa6ca9f4b5d3795c3f85914702d7e886197c1
Anthony Tesija <antesija@gmail.com>
2021-07-12 10:28:28 -0700

    Move new website to new repo

--- react/about-me/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import App from "./components/App";
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
